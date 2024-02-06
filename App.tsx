5a925cac3e67aa5fb2649a425aaa3b2b04407324
jkussoy <s22127003@student.unklab.ac.id>
2024-02-06 21:14:58 +0800

    AwesomeProject

--- App.tsx
+++ App.tsx
@@ -1,118 +1,171 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import React from 'react';
-import type {PropsWithChildren} from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
+  Button,
   Text,
-  useColorScheme,
   View,
+  Image,
+  ScrollView,
+  TextInput,
+  StyleSheet,
+  TouchableOpacity,
 } from 'react-native';
+import React from 'react';
+import Download from './Assets/be-a-Programmer.jpg';
+import code from './Assets/code.jpeg';
+import tech from './Assets/tech.jpeg';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+const App = () => {
+  const handlePress = () => {
+    alert('Welcome to RN');
   };
 
+
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
+    <View style={styles.container}>
+      <Text style={styles.title}>Welcome To Basic Components in RN</Text>
+    <ScrollView>
+      <TextInput
+        style={styles.inputcollum}
+        placeholder="   Masukan Username anda"
+        placeholderTextColor="black"
+        secureTextEntry
       />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+      <TextInput
+        style={styles.inputcollum}
+        placeholder="   Masukan Password Anda"
+        placeholderTextColor="black"
+        secureTextEntry
+      />
+      <TouchableOpacity
+          onPress={handlePress}
+          style={styles.touchableOpacity}>
+          <Text style={styles.touchableText}>submit</Text>
+        </TouchableOpacity>
+        <View style={styles.colorBoxesContainer}>
+          <View style={[styles.colorBox, {backgroundColor: 'black'}]} />
+          <View style={[styles.colorBox, {backgroundColor: 'grey'}]} />
+          <View style={[styles.colorBox, {backgroundColor: 'black'}]} />
+        </View>
+        <Image
+          source={require('./Assets/uk.png')}
+          style={styles.image}
+        />
+        <Text style={styles.uk}>Universitas Klabat</Text>
+        <Image
+          source={{uri: 'https://reactnative.dev/img/tiny_logo.png'}}
+          style={styles.image}
+        />
+        <Text style={styles.rn}>React Native</Text>
+        <Image source={Download} style={styles.image} />
+        <Text style={styles.prog}>Become A Professional Programer</Text>
+        <Text style={styles.ty}>Thankyou For visiting</Text>
+        <View style={styles.colorBoxesContainer1}>
+          <View style={[styles.colorBox1, {backgroundColor: 'black'}]} />
+          <View style={[styles.colorBox1, {backgroundColor: 'grey'}]} />
+          <View style={[styles.colorBox1, {backgroundColor: 'black'}]} />
         </View>
       </ScrollView>
-    </SafeAreaView>
+    </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  Container: {
+    flex: 1,
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  title: {
+    fontSize: 23,
+    fontWeight: 'bold',
+    color: 'black',
+    textAlign: 'center',
+    marginTop: 20,
+    marginBottom: 20,
+    backgroundColor: '#bebebe',
+    borderColor: 'black',
+    borderRadius: 10,
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  inputcollum: {
+    borderColor: 'black',
+    borderWidth: 1,
+    margin: 10,
+    borderRadius: 10,
+    padding: 10,
   },
-  highlight: {
-    fontWeight: '700',
+  colorBoxesContainer: {
+    flexDirection: 'row',
+    marginTop: 20,
+    justifyContent: 'space-around',
+    borderRadius: 20,
+    marginBottom: 20,
+  },
+  colorBox: {
+    marginTop: 5,
+    width: 120,
+    height: 100,
+    borderRadius: 10,
+  },
+  colorBoxesContainer1: {
+    flexDirection: 'row',
+    marginTop: 20,
+    justifyContent: 'space-around',
+    borderRadius: 20,
+    marginBottom: 80,
+  },
+  colorBox1: {
+    marginTop: 5,
+    width: 120,
+    height: 100,
+    borderRadius: 10,
+  },
+  image: {
+    width: 200,
+    height: 200,
+    alignSelf: 'center',
+    marginVertical: 10,
+    borderRadius: 20,
+  },
+  touchableOpacity: {
+    backgroundColor: 'black',
+    padding: 10,
+    borderRadius: 8,
+    alignSelf: 'center',
+    marginTop: 20,
+  },
+  touchableText: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  uk:{
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'black',
+    textAlign: 'center',
+    marginTop: 10,
+    marginBottom: 20,
+  },
+  rn:{
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'black',
+    textAlign: 'center',
+    marginTop: 10,
+    marginBottom: 20,
+  },
+  prog:{
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'black',
+    textAlign: 'center',
+    marginTop: 10,
+    marginBottom: 20,
+  },
+  ty:{
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'black',
+    textAlign: 'center',
+    marginTop: 10,
+    marginBottom: 20,
   },
 });
-
 export default App;
